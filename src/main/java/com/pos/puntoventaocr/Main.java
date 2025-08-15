520b750d11c7a6f2de176c9b2818390b1275a0fd
Jose Puga <2430284@upv.edu.mx>
2025-08-15 11:48:16 -0600

    ProyectoNuevo

--- src/main/java/com/pos/puntoventaocr/Main.java
+++ src/main/java/com/pos/puntoventaocr/Main.java
@@ -1,56 +1,17 @@
-package com.pos.puntoventaocr;
+package org.example;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.stage.Stage;
-import com.pos.puntoventaocr.config.DatabaseConnection;
-
-public class Main extends Application {
-
-    @Override
-    public void start(Stage primaryStage) {
-        try {
-            // Verificar conexión a base de datos
-            if (!DatabaseConnection.testConnection()) {
-                System.err.println("Error: No se pudo conectar a la base de datos");
-                System.exit(1);
-            }
-
-            // Cargar la vista de login
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/login.fxml"));
-            Parent root = loader.load();
-
-            // Configurar la escena
-            Scene scene = new Scene(root);
-            scene.getStylesheets().add(getClass().getResource("/css/styles.css").toExternalForm());
-
-            // Configurar el stage
-            primaryStage.setTitle("Sistema POS - Login");
-            primaryStage.setScene(scene);
-            primaryStage.setResizable(false);
-            primaryStage.centerOnScreen();
-
-            // Si tienes un logo, descomenta esta línea
-            // primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/images/logo.png")));
-
-            primaryStage.show();
-
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.err.println("Error al iniciar la aplicación: " + e.getMessage());
-        }
-    }
-
-    @Override
-    public void stop() {
-        // Cerrar conexiones de base de datos al cerrar la aplicación
-        DatabaseConnection.closeConnection();
-    }
-
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
     public static void main(String[] args) {
-        launch(args);
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            System.out.println("i = " + i);
+        }
     }
 }
